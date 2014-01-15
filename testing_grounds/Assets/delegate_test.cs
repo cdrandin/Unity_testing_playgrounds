8de285d463f40dd4a5ea7f46a68542df092c4b18
cdrandin <cdrandin@hotmail.com>
2014-01-14 23:36:38 -0800

    Put some previous Unity stuff

---
+++ testing_grounds/Assets/delegate_test.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class delegate_test : MonoBehaviour {
+	
+	delegate void PowerUpsDelegate();
+	PowerUpsDelegate powerUps;
+	private Color init_color = Color.gray;
+	
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKey(KeyCode.Space))
+		{
+			powerUps += PowerUp;
+			powerUps += TurnRed;
+			
+			powerUps -= PowerDown;
+			powerUps -= TurnGray;
+		}
+		else
+		{
+			powerUps -= PowerUp;
+			powerUps -= TurnRed;
+			
+			powerUps += PowerDown;
+			powerUps += TurnGray;
+		}
+		
+		if(powerUps != null)
+			powerUps();
+	}
+	void PowerUp ()
+	{
+		
+			//print("Powering up!!!");
+		
+	}
+	void TurnRed ()
+	{
+		renderer.material.color = Color.red;
+	}
+	
+	void PowerDown ()
+	{
+		
+			//print("Powering down!!!");
+		
+	}
+	
+	void TurnGray ()
+	{
+		renderer.material.color = init_color;
+	}
+}
---
+++ testing_grounds/Assets/main.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+//delegate int NumberChanger(int n);
+
+public class EventTest
+{
+	private int value;
+	
+	public delegate void NumManipulationHandler();
+	
+	public event NumManipulationHandler ChangeNum;
+	
+	protected virtual void OnNumChanged()
+	{
+		if (ChangeNum != null)
+		{
+			ChangeNum();
+		}
+		else
+		{
+			Debug.Log(string.Format("Event fired!: {0}", value));
+		}
+	}
+	
+	public EventTest(int n)
+	{
+		SetValue(n);
+	}
+	
+	public void SetValue(int n)
+	{
+		if(value != n)
+		{
+			value = n;
+			OnNumChanged();
+		}
+	}
+}
+	
+public class main : MonoBehaviour 
+{
+	/*
+	static int num = 10;
+	public static int AddNum(int p)
+	{
+		num += p;
+		return num;
+	}
+	
+	public static int MultNum(int q)
+	{
+		num *= q;
+		return num;
+	}
+	
+	public static int getNum()
+	{
+		return num;
+	}
+	
+	// Use this for initialization
+	void Start () 
+	{
+		//create delegate instances
+		NumberChanger nc;
+         NumberChanger nc1 = new NumberChanger(AddNum);
+         NumberChanger nc2 = new NumberChanger(MultNum);
+		
+		nc = nc2;
+		nc += nc1;
+		nc(5);
+		
+		Debug.Log(string.Format("Value of Num: {0}", getNum()));
+	}
+	*/
+	
+	
+	void Start ()
+	{
+		
+		EventTest e = new EventTest(5);
+		e.SetValue(7);
+		e.SetValue(11);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
